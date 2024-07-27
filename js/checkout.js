cda65be9d697f1668e8763be861df6c75f426f5d
karenjaffal01 <114339552+karenjaffal01@users.noreply.github.com>
2024-07-27 06:57:25 +0300

    Update checkout.js

--- js/checkout.js
+++ js/checkout.js
@@ -2,6 +2,7 @@ document.addEventListener('DOMContentLoaded', function() {
     const totalAmount = document.getElementById("amount");
 
     function getTotal() {
+        //once the cart page is done this will get updated to calculate total amount of selected items 
         return 50.00;
     }
 
